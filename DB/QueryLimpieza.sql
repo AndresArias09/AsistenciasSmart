46bb88db151116505f3f0630e5f8c2fff3b72bb6
leonardoArias09 <luribe@smart.edu.co>
2024-03-05 08:13:19 -0500

    Actualizando query de limpieza

--- DB/QueryLimpieza.sql
+++ DB/QueryLimpieza.sql
@@ -1,2 +1,8 @@
 delete from asistencia.colaborador;
 delete from asistencia.registro_asistencia;
+
+truncate table aud.auditoria_descarga_archivos;
+truncate table aud.auditoria_envio_email;
+truncate table aud.auditoria_login_usuario;
+truncate table aud.auditoria_navegacion;
+truncate table seg.registro_otp;
